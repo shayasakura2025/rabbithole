f11ab6a5e2c6ce5a8bc9765d92d7e2072630afcb
LadaMae <ldimascolo@icloud.com>
2025-03-28 19:32:54 -0400

    Eating script is in :salute:

---
+++ Assets/Scripts/EatingScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatingScript : MonoBehaviour
+{
+    [SerializeField] CircleCollider2D playerCollider; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collidedObject = collision.gameObject;
+
+        // Do something with the collided object (e.g., log its name)
+        Debug.Log("Collided with: " + collidedObject.name);
+        float playerSize = playerCollider.bounds.size.magnitude;
+        float objectSize = collision.bounds.size.magnitude;
+        //eat object if player is larger
+        if (playerSize > objectSize)
+        {
+            Debug.Log("ate");
+            playerCollider.radius = playerCollider.radius + (float)0.25;
+            Destroy(collision.gameObject);
+        }
+    }
+}
