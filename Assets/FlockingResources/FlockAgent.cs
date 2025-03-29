4722c17a6df1077f26da340b165b4d9d66991d47
SquishyCode <mjmergal13@gmail.com>
2025-03-29 14:36:59 -0400

    boids 80 percent way there
    im so eepy

---
+++ Assets/FlockingResources/FlockAgent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+
+public class FlockAgent : MonoBehaviour
+{
+
+    Collider2D agentCollider;
+    public Collider2D AgentCollider {get {return agentCollider;}}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    public void Move(Vector2 velocity){
+        
+        transform.up = velocity;
+        transform.position += (Vector3)velocity * Time.deltaTime;
+
+    }
+}
