a64f6438cdbb4304a03d8b19a85971aec7584514
LadaMae <ldimascolo@icloud.com>
2025-03-30 05:23:49 -0400

    Added sounds

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public class MainMenu : MonoBehaviour
 {
+    public AudioClip buttonSound;
+    new AudioSource audio;
     public void PlayGame() {
+        audio = GetComponent<AudioSource>();
+        audio.clip = buttonSound;
+        audio.Play();
+        StartCoroutine(waitForSound());
         SceneManager.LoadScene("GameScene");
     }
 
     public void QuitGame() {
+        audio = GetComponent<AudioSource>();
+        audio.clip = buttonSound;
+        audio.Play();
+        StartCoroutine(waitForSound());
         Application.Quit();
+    }
+
+    public IEnumerator waitForSound()
+    {
+        while (audio.isPlaying)
+            yield return null;
+
     }
 }
